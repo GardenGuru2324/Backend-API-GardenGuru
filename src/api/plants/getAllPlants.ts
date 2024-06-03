5adefe7d5e0c18d3dff34e0b6a31834c185da038
Renzo-Declerck <declerck.renzo@outlook.com>
2024-06-03 16:01:24 +0200

    test push dev

--- src/api/plants/getAllPlants.ts
+++ src/api/plants/getAllPlants.ts
@@ -1,17 +1,18 @@
-import express from 'express';
-import { createResponseObject } from '../../common/common';
-import { getPlants } from '../../database/plants/allPlants';
+import express from "express";
+import { createResponseObject } from "../../common/common";
+import { getPlants } from "../../database/plants/allPlants";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-	try {
-		const allPlants = await getPlants();
+router.get("/", async (req, res) => {
+  try {
+    // test
+    const allPlants = await getPlants();
 
-		return createResponseObject(200, allPlants, res);
-	} catch (error) {
-		return error;
-	}
+    return createResponseObject(200, allPlants, res);
+  } catch (error) {
+    return error;
+  }
 });
 
 export default router;
