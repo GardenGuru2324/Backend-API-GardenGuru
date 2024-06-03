175d92eae7cf685ef72310c0909ef702f035c5c6
Renzo-Declerck <declerck.renzo@outlook.com>
2024-06-03 15:37:16 +0200

    test

--- src/database/users/queryUserByUserId.ts
+++ src/database/users/queryUserByUserId.ts
@@ -1,21 +1,24 @@
-import { connectDatabase, closeDatabase } from '../db';
-import { MongoClient } from 'mongodb';
-import 'dotenv/config';
-import { User } from '../../types/user/user';
+import { connectDatabase, closeDatabase } from "../db";
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+import { User } from "../../types/user/user";
 
 const uri: string = process.env.MONGO_CONNECT_URL!;
 const database: string = process.env.DATABASE!;
 const client = new MongoClient(uri);
 
 export const getUserByUserId = async (
-	userId: string
+  userId: string
 ): Promise<User | unknown> => {
-	try {
-		await connectDatabase();
-		return await client.db(database).collection('Users').findOne({ userId: userId }) as User;
-	} catch (error) {
-		return error;
-	} finally {
-		await closeDatabase();
-	}
+  try {
+    await connectDatabase();
+    return (await client
+      .db(database)
+      .collection("Users")
+      .findOne({ userId: userId })) as User;
+  } catch (error) {
+    return error;
+  } finally {
+    await closeDatabase();
+  }
 };
