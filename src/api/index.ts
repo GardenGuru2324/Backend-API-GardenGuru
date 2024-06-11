b607574cb1bbc5586ac49294d782ec1022be3303
Renzo-Declerck <137225593+Renzo-Declerck@users.noreply.github.com>
2024-06-11 09:18:10 +0200

    feat: adding getPlantByPlantId-endpoint (#18)
    * feat: adding getPlantByPlantId-endpoint
    
    * ref: replacing code with already-defined functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import plant from "./plants/getPlantByPlantId";
 import plantsOfUser from "./plants/getAllPlantsOfUser";
 import allPlants from "./plants/getAllPlants";
 import deletePlantByPlantId from "./plants/deletePlantByPlantId";
@@ -12,14 +13,15 @@ const router = express.Router();
 
 // Achter deze API endpoint zitten al de endpoints. {baseUrl}/api/v1/{api-endpoint}
 router.get("/", (req, res) => {
-	res.json({
-		message: "API V1"
-	});
+  res.json({
+    message: "API V1",
+  });
 });
 
 // Hier worden de endpoints gedefinieerd die gebruikt moeten worden door de router.
 
 // Plants
+router.use(plant);
 router.use(allPlants);
 router.use(plantsOfUser);
 router.use(deletePlantByPlantId);
