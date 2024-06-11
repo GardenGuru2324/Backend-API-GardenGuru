c56d53ce6da7e51edc37bff5dbfbb96fb269dcd3
Renzo-Declerck <declerck.renzo@outlook.com>
2024-06-11 09:17:34 +0200

    ref: replacing code with already-defined functions

--- src/common/plants/common.ts
+++ src/common/plants/common.ts
@@ -1,7 +1,9 @@
 import { NotFoundError } from "../../errors/error";
 import { errorMessages } from "../../errors/errorMessages";
 import { Plant } from "../../types/plant/plant";
+import { isNullOrUndefined } from "../common";
 
 export const doesPlantExist = (plant: Plant) => {
-  if (plant === null) throw new NotFoundError(errorMessages.plantNotFound);
+  if (isNullOrUndefined(plant))
+    throw new NotFoundError(errorMessages.plantNotFound);
 };
