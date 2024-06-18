1b976e3c8ffeae5a11de009e21a3452f05d05c3f
Renzo-Declerck <declerck.renzo@outlook.com>
2024-06-18 09:08:15 +0200

    feat: adding plantLocation-filter where fetching the plants of an user

--- src/api/plants/getAllPlantsOfUser.ts
+++ src/api/plants/getAllPlantsOfUser.ts
@@ -7,33 +7,64 @@ import { errorMessages } from "../../errors/errorMessages";
 import { doesUserExist } from "../../common/users/common";
 import { User } from "../../types/user/user";
 import { queryGetUserByUserId } from "../../database/users/queryGetUserByUserId";
+import { PlantLocation } from "../../types/plantLocation/plantLocation";
+import { queryGetPlantLocationByLocationName } from "../../database/plantLocations/queryGetPlantLocationByLocationName";
+import { doesPlantLocationExist } from "../../common/plants/common";
 
 const router = express.Router();
 
 router.get("/user/:userId/plants", async (req, res) => {
-	const userId = req.params.userId;
-	try {
-		await validateUser(userId);
+  const userId = req.params.userId;
+  const plantLocationName: string | undefined = req.query.location as
+    | string
+    | undefined;
+  try {
+    await validateUser(userId);
 
-		const allPlantsOfUser: Plant[] = (await queryGetAllPlantsOfUser(userId)) as Plant[];
+    let allPlantsOfUser: Plant[] = (await queryGetAllPlantsOfUser(
+      userId
+    )) as Plant[];
 
-		checkIfUserHasPlants(allPlantsOfUser);
+    checkIfUserHasPlants(allPlantsOfUser);
+    if (plantLocationName !== undefined && plantLocationName !== "") {
+      const plantLocation: PlantLocation = await validatePlantLocation(
+        plantLocationName
+      );
+      allPlantsOfUser = allPlantsOfUser.filter(
+        (plant: Plant) => plant.locationId === plantLocation.locationId
+      );
+      checkIfUserHasPlantsOfLocation(allPlantsOfUser);
+    }
 
-		return createResponseObject(200, allPlantsOfUser, res);
-	} catch (error) {
-		return handleErrors(error, res);
-	}
+    return createResponseObject(200, allPlantsOfUser, res);
+  } catch (error) {
+    return handleErrors(error, res);
+  }
 });
 
 const validateUser = async (userId: string) => {
-	const user: User = (await queryGetUserByUserId(userId)) as User;
-	doesUserExist(user);
+  const user: User = (await queryGetUserByUserId(userId)) as User;
+  doesUserExist(user);
+};
+
+const validatePlantLocation = async (
+  locationName: string
+): Promise<PlantLocation> => {
+  const plantLocation: PlantLocation =
+    (await queryGetPlantLocationByLocationName(locationName)) as PlantLocation;
+  doesPlantLocationExist(plantLocation);
+  return plantLocation;
 };
 
 const checkIfUserHasPlants = (allPlantsOfUser: Plant[]) => {
-	if (allPlantsOfUser.length === 0) {
-		throw new ConflictError(errorMessages.userHasNoPlants);
-	}
+  if (allPlantsOfUser.length === 0) {
+    throw new ConflictError(errorMessages.userHasNoPlants);
+  }
+};
+
+const checkIfUserHasPlantsOfLocation = (allPlantsOfUserOfLocation: Plant[]) => {
+  if (allPlantsOfUserOfLocation.length === 0)
+    throw new ConflictError(errorMessages.userHasNoPlantsOfLocation);
 };
 
 export default router;
--- src/errors/errorMessages.ts
+++ src/errors/errorMessages.ts
@@ -2,7 +2,12 @@ export const errorMessages = {
   userNotFound: "User not found",
   userHasNoPlants:
     "You currently do not have any plants in your collection. Please add a plant to get started.",
-  plantNotFound: "Plant not found. Please verify the plant ID and try again. Note: You may not be the owner of this plant.",
+  userHasNoPlantsOfLocation:
+    "You currently do not have any plants of this location.",
+  plantLocationNotFound:
+    "The type of location does not exist. Please use one of the defined location types.",
+  plantNotFound:
+    "Plant not found. Please verify the plant ID and try again. Note: You may not be the owner of this plant.",
   userAlreadyExist:
     "A user with this email already exist. Please check your email or try to log in.",
   invalidEmail: "Email is not valid. Please try again!",
