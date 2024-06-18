59bfbb832ab6485021ef21eb8ac6fddba8fb21e6
Renzo-Declerck <137225593+Renzo-Declerck@users.noreply.github.com>
2024-06-18 12:29:32 +0200

    Feat/guru 30 (#20)
    Merge feat/guru30 into dev

--- src/common/plants/common.ts
+++ src/common/plants/common.ts
@@ -1,9 +1,15 @@
 import { NotFoundError } from "../../errors/error";
 import { errorMessages } from "../../errors/errorMessages";
 import { Plant } from "../../types/plant/plant";
+import { PlantLocation } from "../../types/plantLocation/plantLocation";
 import { isNullOrUndefined } from "../common";
 
 export const doesPlantExist = (plant: Plant) => {
   if (isNullOrUndefined(plant))
     throw new NotFoundError(errorMessages.plantNotFound);
+};
+
+export const doesPlantLocationExist = (plantLocation: PlantLocation) => {
+  if (isNullOrUndefined(plantLocation))
+    throw new NotFoundError(errorMessages.plantLocationNotFound);
 };
