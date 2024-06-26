b64681fee4b38df4071a2b404a588a51db59ed62
Renzo-Declerck <declerck.renzo@outlook.com>
2024-06-26 11:56:02 +0200

    test: removing console loggings of test purposes

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,8 +3,7 @@ import 'dotenv/config';
 
 const uri: string = process.env.MONGO_CONNECT_URL!;
 const client = new MongoClient(uri);
-console.log(uri);
-console.log(client);
+
 export const connectDatabase = async (): Promise<MongoClient> =>
 	await client.connect();
 
