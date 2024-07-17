96af57ccb08b01b34e71d08e0a0b64c1fca06a71
Renzo-Declerck <declerck.renzo@outlook.com>
2024-07-17 07:44:06 +0200

    fix: deleting not-used common-function

--- src/common/plants/common.ts
+++ src/common/plants/common.ts
@@ -1,15 +1,8 @@
-import { queryGetPlantByPlantName } from '../../database/plants/queryGetPlantByPlantName';
 import { NotFoundError } from '../../errors/error';
 import { errorMessages } from '../../errors/errorMessages';
 import { Plant } from '../../types/plant/plant';
 import { PlantLocation } from '../../types/plantLocation/plantLocation';
 import { isNullOrUndefined } from '../common';
-
-export const validatePlant = async (plantName: string): Promise<Plant> => {
-	const plant: Plant = (await queryGetPlantByPlantName(plantName)) as Plant;
-	doesPlantExist(plant);
-	return plant;
-};
 
 export const doesPlantExist = (plant: Plant): void => {
 	if (isNullOrUndefined(plant))
