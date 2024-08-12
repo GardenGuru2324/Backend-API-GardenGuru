2fdf5b5df22c1eb961bf319ac1a8c51815a41722
MilanMareels <milan.mareels@gmail.com>
2024-08-12 14:40:36 +0200

    fixed pr linter from error to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'warn',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       'import/order': [
         2,
         {
