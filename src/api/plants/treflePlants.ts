f0f6d3311f97c05e47b068ec3f986c9a6aedc4d9
MilanMareels <milan.mareels@gmail.com>
2024-08-22 11:53:41 +0200

    fixed trefle

--- src/api/plants/treflePlants.ts
+++ src/api/plants/treflePlants.ts
@@ -11,26 +11,41 @@ const trefle_api_token: string = process.env.TREFLE_API_TOKEN!;
 
 router.get('/treflePlants', async (req, res) => {
 	const page = parseInt(req.query.page as string) || 1;
-	const plantName: string | undefined = req.query.search as
-		| string
-		| undefined;
-	try {
-		const response = await axios.get(
-			`https://trefle.io/api/v1/plants?token=${trefle_api_token}&page=${page}`,
-		);
+	const plantName: string = req.query.search as string;
 
-		let treflePlants = response.data.data;
+	try {
+		let treflePlants: any[] = [];
+		let response;
+
+		for (let index = 1; index <= 25; index++) {
+			response = await axios.get(
+				`https://trefle.io/api/v1/plants?token=${trefle_api_token}&page=${index}`,
+			);
+			treflePlants = [...treflePlants, response.data.data].flat();
+		}
+		let filtertArray: any[] = [];
 
 		if (plantName !== undefined && plantName !== '') {
-			treflePlants = treflePlants.filter(
-				(plant: { common_name: string }) =>
-					plant.common_name
-						.toLowerCase()
-						.includes(plantName.toLowerCase()),
-			);
+			for (let index = 1; index <= 5; index++) {
+				let test1: number = 0;
+				let test2: number = 50;
+
+				treflePlants = treflePlants
+					.slice(test1, test2)
+					.filter((plant) => plant.common_name === plantName);
+
+				filtertArray = [...filtertArray, treflePlants];
+				test1 += 100;
+				test2 += 100;
+			}
 		}
 
-		return createResponseObject(200, treflePlants, res);
+		const itemsPerPage = 25;
+		const startIndex = (page - 1) * itemsPerPage;
+		const eindIndex = startIndex + itemsPerPage;
+		filtertArray = treflePlants.slice(startIndex, eindIndex);
+
+		return createResponseObject(200, filtertArray, res);
 	} catch (error) {
 		return handleErrors(error, res);
 	}
