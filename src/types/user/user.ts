164d6fe5090da07948de5016266a34cfe3bd892f
MilanMareels <milan.mareels@gmail.com>
2024-08-24 18:41:44 +0200

    deleted userName of interrface

--- src/types/user/user.ts
+++ src/types/user/user.ts
@@ -3,7 +3,6 @@ import { ObjectId } from 'mongodb';
 export interface User {
 	_id: ObjectId;
 	userId: string;
-	userName: string;
 	password: string;
 	rattingPlaced: boolean;
 	profilePicture: string;
